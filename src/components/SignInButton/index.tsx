6c5b2ce620cb3674e5c9a31c30b67edb5551eb3a
Abner Willclefy <abnerwill.monteiro@gmail.com>
2021-10-04 23:51:31 -0300

    :construction: Applied next-auth with github and faunadb connection

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,28 @@
 import { FaGithub } from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import Styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [ session ] = useSession()
 
-  return isUserLoggedIn ? (
+  console.log(session)
+
+  return session ? (
     <button 
       type='button'
-      className={Styles.signInButton}>
+      className={Styles.signInButton}
+      onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Abner Willclefy
+      {session.user.name}
       <FiX color="#737380" className={Styles.closeIcon} />
     </button>
   ) : (
     <button 
       type='button'
-      className={Styles.signInButton}>
+      className={Styles.signInButton}
+      onClick={() => signIn('github')}>
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
