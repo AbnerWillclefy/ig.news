f7c7df070a00fde7dd66e358b24f73b2638f97fa
Abner Willclefy <abnerwill.monteiro@gmail.com>
2021-10-07 23:55:12 -0300

    :sparkles: Stripe subscription process added

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -7,8 +7,6 @@ import Styles from './styles.module.scss'
 export function SignInButton() {
   const [ session ] = useSession()
 
-  console.log(session)
-
   return session ? (
     <button 
       type='button'
