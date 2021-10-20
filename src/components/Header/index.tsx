af56f2638e045ea5d6c828c5aed7bfbe8ad20eef
Abner Willclefy <abnerwill.monteiro@gmail.com>
2021-10-20 00:06:54 -0300

    :sparkles: Added Prismic CMS integration

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,25 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
 import { SignInButton } from '../SignInButton'
 import Styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink'
 
 export function Header() {
+  const { asPath } = useRouter()
+
   return (
     <header className={Styles.headerContainer}>
       <div className={Styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a href="" className={Styles.active}>Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={Styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={Styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
